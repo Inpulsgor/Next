a65d8f38ccf7c241348e4b486513ae6685df9e9c
Inpuls <c4.tm.gor@gmail.com>
2021-08-25 19:35:13 +0300

    rating component ready

--- components/Rating/index.tsx
+++ components/Rating/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, KeyboardEvent } from 'react';
 import cn from 'classnames';
-import StarIcon from '../../public/star.svg';
 import { IProps } from './props';
+import StarIcon from '../../public/star.svg';
 import styles from './styles.module.css';
 
 const Rating = ({ isEditable = false, rating, setRating, ...props }: IProps): JSX.Element => {
@@ -14,19 +14,18 @@ const Rating = ({ isEditable = false, rating, setRating, ...props }: IProps): JS
 	const createRating = (currentRating: number) => {
 		const updatedArray = ratingArray.map((rate: JSX.Element, index: number) => {
 			return (
-				<li key={index} className={cn(styles.star, {
-					[styles.filled]: index < currentRating,
-					[styles.editable]: isEditable
+				<StarIcon
+					key={index}
+					className={cn(styles.star, {
+						[styles.filled]: index < currentRating,
+						[styles.editable]: isEditable
 					})}
 					onMouseEnter={() => hoverRating(index + 1)}
 					onMouseLeave={() => hoverRating(rating)}
 					onClick={() => handleClick(index + 1)}
-				>
-					<StarIcon
-						onKeyDown={(e: KeyboardEvent<SVGElement>) => handeKeyPress(e, index + 1)}
-						tabIndex={isEditable ? 0 : -1}
-					/>
-				</li>
+					onKeyDown={(e: KeyboardEvent<SVGElement>) => handeKeyPress(e, index + 1)}
+					tabIndex={isEditable ? 0 : -1}
+				/>
 			);
 		});
 
@@ -45,13 +44,14 @@ const Rating = ({ isEditable = false, rating, setRating, ...props }: IProps): JS
 
 	const handeKeyPress = (e: KeyboardEvent<SVGElement>, rate: number) => {
 		if (e.code !== "Space" || !setRating) return;
-
 		setRating(rate);
 	};
 
 	return (
 		<ul className={styles.list} {...props}>
-			{ratingArray.map((rate) => (rate))}
+			{ratingArray.map((rate, index) => (
+				<li className={styles.item} key={index}>{rate}</li>
+			))}
 		</ul>
 	);
 };
